44992259412000943099431ae8502ec4e6ea84a0
bea <beatriztattooer@gmail.com>
2022-05-12 22:16:03 -0300

    yeah yeah yeahs! it's working!

--- Tamagotchi.js
+++ Tamagotchi.js
@@ -1,4 +1,12 @@
-export default class Tamagotchi{
+/* tamagotchi status */
+var health = document.getElementById('health');
+var fun = document.getElementById('fun');
+var hungry = document.getElementById('hungry');
+var bath = document.getElementById('bath');
+var poop = document.getElementById('poop');
+var sleep = document.getElementById('sleep');
+
+class Tamagotchi{
     constructor(){
         this.name = 'Baby Shark'
         this.type = 'shark'
@@ -8,7 +16,7 @@ export default class Tamagotchi{
         this.health = 1000
         this.poopNeed = 1000
         this.fun = 1000
-        this.sleep = 0
+        this.sleepy = 0
     }
 
     hello() {
@@ -16,41 +24,75 @@ export default class Tamagotchi{
     }
 
     feed(){
-        console.log('feed! :<')
-        return this.hungry +=100 
+        this.hungry +=100
+        this.update() 
+        return
     }
     cure(){
-        return this.health += 100
+        this.health += 100
+        this.update()
+        return
     }
     bath(){
         this.dirty += 250
         this.health += 150
         this.hp += 50
-        this.sleep -= 50
+        this.sleepy -= 50
+        this.update()
         return
     }
     poop(){
         this.dirty -= 100
         this.health += 100
+        this.update()
         return
     }
     sleep(){
-        this.sleep = 0
+        this.sleepy = 0
         this.fun +=50
         this.health +=250
         this.hungry -= 250
         this.hp += 5
+        this.update()
         return
     }
     read(){
         this.fun += 250
-        this.sleep += 100
+        this.sleepy += 100
+        this.update()
         return
     }
     dance(){
         this.fun +=250
         this.hp += 100
         this.dirty -= 100
+        this.update()
+        return
+    }
+
+    update(){
+        console.log('atualizando!')
+        health.innerHTML = myTamagotchi.health;
+        fun.innerHTML = myTamagotchi.fun;
+        hungry.innerHTML = myTamagotchi.hungry;
+        bath.innerHTML = myTamagotchi.dirty;
+        poop.innerHTML = myTamagotchi.poopNeed;
+        sleep.innerHTML = myTamagotchi.sleepy;
         return
     }
 }
+
+console.log('Game started!')
+console.log('Creating your tamagotchi...')
+const myTamagotchi = new Tamagotchi();
+
+/* tamagotchi status */
+health.innerHTML = myTamagotchi.health;
+fun.innerHTML = myTamagotchi.fun;
+hungry.innerHTML = myTamagotchi.hungry;
+bath.innerHTML = myTamagotchi.dirty;
+poop.innerHTML = myTamagotchi.poopNeed;
+sleep.innerHTML = myTamagotchi.sleepy;
+
+console.log('Congratulations! Your tamagotchi was born :)')
+console.log(myTamagotchi)
